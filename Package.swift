1bfd8f9acd046e957e200e1fcf44ea8ae01809ac
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2025-10-14 20:08:27 +0200

    u

--- Package.swift
+++ Package.swift
@@ -14,12 +14,54 @@ let package = Package(
     ],
     targets: [
         .target(
+            name: "BlufiLibraryObjC",
+            dependencies: [],
+            path: "ios/Sources/BlufiPlugin",
+            exclude: [
+                "Blufi.swift",
+                "BlufiPlugin.swift",
+                "BlufiPlugin-Bridging-Header.h"
+            ],
+            sources: [
+                "BlufiLibrary",
+                "ESPAPPResources"
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("BlufiLibrary"),
+                .headerSearchPath("BlufiLibrary/Data"),
+                .headerSearchPath("BlufiLibrary/Response"),
+                .headerSearchPath("BlufiLibrary/Security"),
+                .headerSearchPath("ESPAPPResources"),
+                .unsafeFlags(["-Wno-error=deprecated-declarations"])
+            ],
+            linkerSettings: [
+                .linkedLibrary("ssl", .when(platforms: [.iOS])),
+                .linkedLibrary("crypto", .when(platforms: [.iOS]))
+            ]
+        ),
+        .target(
             name: "BlufiPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                "BlufiLibraryObjC"
             ],
-            path: "ios/Sources/BlufiPlugin"),
+            path: "ios/Sources/BlufiPlugin",
+            exclude: [
+                "BlufiLibrary",
+                "ESPAPPResources",
+                "BlufiPlugin-Bridging-Header.h"
+            ],
+            sources: [
+                "Blufi.swift",
+                "BlufiPlugin.swift"
+            ],
+            cSettings: [
+                .headerSearchPath("BlufiLibrary")
+            ]
+        ),
         .testTarget(
             name: "BlufiPluginTests",
             dependencies: ["BlufiPlugin"],
---
+++ ios/Sources/BlufiPlugin/include/BlufiLibraryObjC.h
@@ -0,0 +1,24 @@
+//
+//  BlufiLibraryObjC.h
+//  Umbrella header for BluFi Objective-C Library
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+// BluFi Library
+#import "../BlufiLibrary/BlufiClient.h"
+#import "../BlufiLibrary/BlufiConfigureParams.h"
+#import "../BlufiLibrary/BlufiConstants.h"
+#import "../BlufiLibrary/BlufiScanResponse.h"
+#import "../BlufiLibrary/BlufiStatusResponse.h"
+#import "../BlufiLibrary/BlufiVersionResponse.h"
+#import "../BlufiLibrary/Data/BlufiFrameCtrlData.h"
+#import "../BlufiLibrary/Data/BlufiNotifyData.h"
+#import "../BlufiLibrary/Security/BlufiSecurity.h"
+#import "../BlufiLibrary/Security/BlufiDH.h"
+
+// ESP Resources
+#import "../ESPAPPResources/ESPPeripheral.h"
+#import "../ESPAPPResources/ESPFBYBLEHelper.h"
+#import "../ESPAPPResources/ESPDataConversion.h"
