28fbf1a0bbb587118f857daae39db5aac417c684
Silas Martin <silas@solakon.de>
2026-04-28 19:08:41 +0200

    fix: revert version name change

--- android/src/main/java/de/codext/capacitor/blufi/BlufiClient.java
+++ android/src/main/java/de/codext/capacitor/blufi/BlufiClient.java
@@ -11,7 +11,7 @@ import de.codext.capacitor.blufi.response.BlufiStatusResponse;
 import de.codext.capacitor.blufi.response.BlufiVersionResponse;
 
 public class BlufiClient {
-    public static final String VERSION = BuildConfig.VERSION_NAME;
+    public static final String VERSION = "1.0.0";
 
     private final BlufiClientImpl mImpl;
 
